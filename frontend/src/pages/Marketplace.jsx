19ec739fe5af03271f431f9b29e7cce10e695592
Rezka08 <rreska9@gmail.com>
2025-11-22 13:17:23 +0800

    fix: Add wishlist cleanup logic in Marketplace to remove invalid IDs

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -29,10 +29,25 @@ const Marketplace = () => {
     fetchWastes();
   }, []);
 
+  // Clean up wishlist - remove IDs that no longer exist in available wastes
+  useEffect(() => {
+    if (wastes.length > 0 && wishlist.length > 0) {
+      const availableWasteIds = wastes.map(w => w.id);
+      const validWishlist = wishlist.filter(id => availableWasteIds.includes(id));
+
+      // Update if there are invalid IDs in wishlist
+      if (validWishlist.length !== wishlist.length) {
+        setWishlist(validWishlist);
+        localStorage.setItem('wishlist', JSON.stringify(validWishlist));
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wastes]);
+
   useEffect(() => {
     filterWastes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedCategory, searchTerm, wastes, view]);
+  }, [selectedCategory, searchTerm, wastes, view, wishlist]);
 
   const fetchWastes = async () => {
     try {
